dd35c233ef71623fd3adef27ab5b2f5a78aea9f4
Michal Jazwinski <mail@mihow.pl>
2025-09-09 11:09:47 +0200

    Switch to HashRouter and configure base path for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Page1 from "./Page1.tsx";
 import Page2 from "./Page2.tsx";
@@ -6,13 +6,13 @@ import Page2 from "./Page2.tsx";
 function App() {
 
   return (
-    <Router>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<Navigate to="/page1" />} />
             <Route path="/page1" element={<Page1/>} />
             <Route path="/page2" element={<Page2/>} />
         </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
