87fc6cf49992987ee91e436edf3b48f52b37b6b9
Michal Jazwinski <mail@mihow.pl>
2025-09-09 11:18:35 +0200

    Switch to BrowserRouter, update Azure Static Web Apps workflow configuration, and add navigation fallback.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Page1 from "./Page1.tsx";
 import Page2 from "./Page2.tsx";
@@ -6,13 +6,13 @@ import Page2 from "./Page2.tsx";
 function App() {
 
   return (
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
             <Route path="/" element={<Navigate to="/page1" />} />
             <Route path="/page1" element={<Page1/>} />
             <Route path="/page2" element={<Page2/>} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
